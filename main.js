979cf8b1e8128dcddd315de7d05e1028743a0d54
akh-ild <ildar.akhmadeyev@gmail.com>
2023-02-01 17:54:34 +0600

    days in month, weekday

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const mthEl = document.querySelector('.mth');
 const nextMthEl = document.querySelector('.next-mth');
 const prevMthEl = document.querySelector('.prev-mth');
 const daysEl = document.querySelector('.days');
+const weekEl = document.querySelector('.week');
+let dayEl;
+let emptyDayEl;
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -18,10 +21,12 @@ let selectedDay = day;
 let selectedMonth = month;
 let selectedYear = year;
 
-mthEl.textContent = months[month] + ' ' + year;
-selectedDateEl.textContent = formatDate(day, month, year);
-
 // Functions
+function setTodaysDate() {
+  mthEl.textContent = months[month] + ' ' + year;
+  selectedDateEl.textContent = formatDate(day, month, year);
+}
+
 function toggleDatePicker(e) {
   datesEl.classList.toggle('active');
 }
@@ -52,31 +57,58 @@ function formatDate(day, month, year) {
   return `${day} / ${month} / ${year}`;
 }
 
+function getDaysInMonth(year, month) {
+  let amountDays = new Date(year, month + 1, 0).getDate();
+  return amountDays;
+}
+
+function populateWeekDays() {
+  const weekDays = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+  weekDays.forEach((day) => {
+    const weekDayEl = document.createElement('div');
+    weekDayEl.classList.add('week-day');
+    weekDayEl.textContent = day;
+    weekEl.appendChild(weekDayEl);
+  });
+}
+
 function populateDates(e) {
   daysEl.innerHTML = '';
-  let amountDays = 31;
-  if (month === 1) amountDays = 28;
-  for (let i = 0; i < amountDays; i++) {
-    const dayEl = document.createElement('div');
-    dayEl.classList.add('day');
-    dayEl.textContent = i + 1;
+  console.log(month, year);
+  let firstDayMonth = new Date(year, month, 1).getDay();
+  console.log(firstDayMonth);
+  for (let i = 0; i < firstDayMonth; i++) {
+    emptyDayEl = document.createElement('div');
+    emptyDayEl.classList.add('empty');
+    daysEl.appendChild(emptyDayEl);
+  }
+  for (let i = 0; i < getDaysInMonth(year, month, 0); i++) {
+      dayEl = document.createElement('div');
+      dayEl.classList.add('day');
+      dayEl.textContent = i + 1;
     if (selectedDay == (i + 1) && selectedMonth === month && selectedYear === year) {
       dayEl.classList.add('selected');
     }
-    dayEl.addEventListener('click', () => {
-      selectedDate = new Date(year + '-' + (month + 1) + '-' + (i + 1));
-      selectedDay = (i + 1);
-      selectedMonth = month;
-      selectedYear = year;
-      selectedDateEl.textContent = formatDate(selectedDay, selectedMonth, selectedYear);
-      selectedDateEl.dataset.value = selectedDate;
-      toggleDatePicker();
-      populateDates();
-    });
+    setDayEventListener(i);
     daysEl.appendChild(dayEl);
   }
 }
 
+function setDayEventListener(day) {
+  dayEl.addEventListener('click', () => {
+    selectedDate = new Date(year + '-' + (month + 1) + '-' + (day + 1));
+    selectedDay = (day + 1);
+    selectedMonth = month;
+    selectedYear = year;
+    selectedDateEl.textContent = formatDate(selectedDay, selectedMonth, selectedYear);
+    selectedDateEl.dataset.value = selectedDate;
+    toggleDatePicker();
+    populateDates();
+  });
+}
+
+setTodaysDate();
+populateWeekDays();
 populateDates();
 
 // Event listeners
